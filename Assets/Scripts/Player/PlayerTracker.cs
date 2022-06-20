524d634aec84875bda9fee076342ff43065c0772
Ivan <39524682+Skrals@users.noreply.github.com>
2022-06-20 13:00:12 +0300

    Player tracker

---
+++ Assets/Scripts/Player/PlayerTracker.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class PlayerTracker : MonoBehaviour
+{
+    [SerializeField] private Player _player;
+    [SerializeField] private float _speed;
+
+    private void FixedUpdate()
+    {
+        transform.position = Vector3.Lerp(transform.position, GetTargetPosition(), _speed * Time.fixedDeltaTime);
+    }
+
+    private Vector3 GetTargetPosition()
+    {
+        return new Vector3(_player.transform.position.x, _player.transform.position.y, transform.position.z);
+    }
+}
