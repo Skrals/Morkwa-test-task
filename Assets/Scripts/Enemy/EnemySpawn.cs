78e291cf27e35de4c1a08524a670a0e22529e7c3
Ivan <39524682+Skrals@users.noreply.github.com>
2022-06-21 14:01:23 +0300

    Update EnemySpawn.cs

--- Assets/Scripts/Enemy/EnemySpawn.cs
+++ Assets/Scripts/Enemy/EnemySpawn.cs
@@ -35,15 +35,16 @@ public class EnemySpawn : MonoBehaviour
             _pointX = rand.Next(_maze.GetLength(0) - 2);
             _pointY = rand.Next(_maze.GetLength(1) - 2);
 
-            if(_maze[_pointX, _pointY].BlockEnabled == false)
+            if (_maze[_pointX, _pointY].BlockEnabled == false)
             {
                 break;
+            }
         }
+
     }
 
-            }
     private void SpawnEnemy(Enemy enemy, int x, int y)
     {
-        Instantiate(enemy, new Vector2(x,y), Quaternion.identity);
+        Instantiate(enemy, new Vector2(x, y), Quaternion.identity);
     }
 }
