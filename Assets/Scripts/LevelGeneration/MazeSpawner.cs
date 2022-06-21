c592d70e34076c915549767b92557be294aa52cb
Ivan <39524682+Skrals@users.noreply.github.com>
2022-06-21 13:33:08 +0300

    TODO

--- Assets/Scripts/LevelGeneration/MazeSpawner.cs
+++ Assets/Scripts/LevelGeneration/MazeSpawner.cs
@@ -10,6 +10,7 @@ public class MazeSpawner : MonoBehaviour
     {
         MazeGenerator generator = new MazeGenerator();
         MazeGeneratorCell[,] maze = generator.GenerateMaze();
+
         for (int x = 0; x < maze.GetLength(0); x++)
         {
             for (int y = 0; y < maze.GetLength(1); y++)
