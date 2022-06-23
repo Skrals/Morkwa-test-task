3d877a35108198fa7834c8f7b57406b218efbaa3
Ivan <39524682+Skrals@users.noreply.github.com>
2022-06-23 10:59:16 +0300

    Main menu

---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayButton()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitButton()
+    {
+        Application.Quit();
+    }
+}
